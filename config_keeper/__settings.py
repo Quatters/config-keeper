aa826a0c1f1eeb8e27c45f4384725127a9be4737
Vladislav Korenkov <vladnfs3@gmail.com>
2023-10-13 23:46:06 +1000

    Use get_app_dir for config file

--- config_keeper/__settings.py
+++ config_keeper/__settings.py
@@ -1,8 +1,12 @@
 import os
 from pathlib import Path
 
+import typer
+
 # paths
-_DEFAULT_CONFIG_FILE = Path.home() / '.config-keeper.yaml'
+_DEFAULT_CONFIG_FILE = (
+    Path(typer.get_app_dir('config-keeper', roaming=False)) / 'config.yaml'
+)
 CONFIG_FILE = Path(os.getenv('CONFIG_KEEPER_CONFIG_FILE', _DEFAULT_CONFIG_FILE))
 
 # etc
